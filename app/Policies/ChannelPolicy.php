3e04002c2d3324c35fa04d75fcad87a05ac73887
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-11 16:43:58 +0200

    policy for channel model

---
+++ app/Policies/ChannelPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Channel;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ChannelPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user, Channel $channel)
+    {
+        return $user->id === $channel->user_id;
+    }
+}
