5f03ae5cd33550782fc0fe41c52573020da0908d
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-12 13:31:12 +0200

    nav style

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark elegant-color">
+<nav class="navbar navbar-expand-lg navbar-dark stylish-color-dark">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
