662b318b537ffa09fb94f32cb91b7af00d911c93
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-18 17:04:51 +0200

    mdb bootstrap via npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,5 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css').browserSync('codetube.test');
