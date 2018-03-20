fd0520deb4b47c1bcc358b4cb78585a352e543ef
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-20 12:18:17 +0200

    issue fixed with opening all modals

--- resources/assets/js/components/Edit.vue
+++ resources/assets/js/components/Edit.vue
@@ -1,15 +1,53 @@
 
   <template>
-        <div>
-          <a @click="show"   class="btn btn-outline-default btn-sm ">Edit</a>
+        <div style="display:inline-block">
+           <a @click="show"    class="btn btn-outline-default btn-sm ">Edit</a>
           <transition enter-active-class="animated fadeInUpBig" leave-active-class="animated shake" mode="out-in" appear>
 
 
-          <modal name="update"    >
-            <input id="name" type="text" class="form-control" name="name"           v-model="video.title" required autofocus>
-  
-                  </modal>
-                  </transition>
+          <modal :name="`update-${video.id}`"    >
+               <form @submit.prevent="update">
+                           
+                          
+            <div class="modal-header " >
+                    <h5 class="modal-title" style="text-align:center;">Update  </h5>
+            </div>
+               <div class="card-body " style="background:#fbfbfc"  >
+                   
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" name="title" 
+                             v-model="video.title">
+                          
+                        </div>
+                        <div class="form-group">
+                                <label for="description">Descrition</label>
+                                <textarea name="description" id="description" class="form-control" v-model="video.description">  
+                                </textarea>
+                        
+                            </div>
+                            <div class="form-group">
+                              <label for="visibility">Visibility</label>
+                                    <select name="visibility" id="visibility" class="form-control"  > 
+                                    </select>
+                            
+                                </div>
+                                <div class="form-group">
+                                    <label for="allow_votes">
+                                        <input type="checkbox" name="allow_votes" id="allow_votes"  > Allow votes
+                                    </label>
+                                </div>
+                                <div class="form-group">
+                                        <label for="allow_comments">
+                                            <input type="checkbox" name="allow_comments" id="allow_comments"  > Allow comments
+                                        </label>
+                                    </div>
+                                    <button  >Update</button>
+                                    </div>
+                        </form>
+
+          </modal>
+        </transition>
         </div>
         
   </template>
@@ -20,12 +58,17 @@ export default {
   data() {
     return {
       feedback: "",
-      loading: false
+      loading: false,
+      form: {
+        title: "",
+        description: "",
+        visibility: ""
+      }
     };
   },
   methods: {
     show() {
-      this.$modal.show("update");
+      this.$modal.show(`update-${this.video.id}`);
     }
   }
 };
